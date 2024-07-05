62396a3535698b078b21127fdf3a5b0737c80925
shriVATSA54 <116296557+shriVATSA54@users.noreply.github.com>
2024-07-05 19:42:39 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const constantInstance = await //mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-            mongoose.connect("mongodb+srv://shrivatsaddesai:xZflJ06eo1watYqH@shrimongo.yoc63sm.mongodb.net");
+            mongoose.connect("");
         console.log(`Mongoose Connected ${constantInstance}`);
 
     } catch (error) {
